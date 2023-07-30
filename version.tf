a26b63ae787c3991cb306f8886a202e1000ccdd0
dennartempest@aol.com <salientcrossing@outlook.com>
2023-07-30 14:24:39 -0600

    transit commit

--- version.tf
+++ version.tf
@@ -1,8 +1,12 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0.0"
+#     }
+#   }
+# }
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0.0"
-    }
-  }
+  backend "s3" {}
 }
